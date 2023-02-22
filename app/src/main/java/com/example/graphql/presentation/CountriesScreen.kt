f701f5554e7d267ffc88fe7b92e8e01285ccefd2
Michael Widlok <michael.widlok@immowelt.de>
2023-02-22 16:23:53 +0100

    add detail dialog

--- app/src/main/java/com/example/graphql/presentation/CountriesScreen.kt
+++ app/src/main/java/com/example/graphql/presentation/CountriesScreen.kt
@@ -1,16 +1,23 @@
 package com.example.graphql.presentation
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import com.example.graphql.domain.model.DetailedCountry
 import com.example.graphql.domain.model.SimpleCountry
 
 @Composable
@@ -38,6 +45,17 @@ fun CountriesScreen(
                     )
                 }
             }
+
+            if (state.selectedCountry != null) {
+                CountryDialog(
+                    country = state.selectedCountry,
+                    onDismiss = onDismissCountryDialog,
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(5.dp))
+                        .background(Color.White)
+                        .padding(16.dp)
+                )
+            }
         }
     }
 }
@@ -58,8 +76,42 @@ private fun CountryItem(
             modifier = Modifier.weight(1f)
         ) {
             Text(text = country.name, fontSize = 24.sp)
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.width(16.dp))
             Text(text = country.capital)
+        }
+    }
+}
+
+@Composable
+private fun CountryDialog(
+    country: DetailedCountry,
+    onDismiss: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val joinedLanguages = remember(country.languages) {
+        country.languages.joinToString()
+    }
+
+    Dialog(onDismissRequest = onDismiss) {
+        Column(
+            modifier = modifier
+        ) {
+            Row(
+                modifier = modifier.fillMaxWidth()
+            ) {
+                Text(text = country.emoji, fontSize = 30.sp)
+                Spacer(modifier = Modifier.width(16.dp))
+                Text(text = country.name, fontSize = 24.sp)
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = "Continent: " + country.continent)
+            Spacer(Modifier.height(8.dp))
+            Text(text = "Currency: " + country.currency)
+            Spacer(Modifier.height(8.dp))
+            Text(text = "Capital: " + country.capital)
+            Spacer(Modifier.height(8.dp))
+            Text(text = "Language(s): $joinedLanguages")
+            Spacer(Modifier.height(8.dp))
         }
     }
 }
